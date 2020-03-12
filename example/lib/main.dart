339dda538271be69adc1e1ebf92d4eba5c1fef0a
Andrey <andrey.glushko@ladevelopers.com>
2020-03-12 04:42:43 +0200

    implement calendart phase1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,24 +1,223 @@
 import 'package:calendart/calendart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(home: MyHomePage());
+  Widget build(BuildContext context) => MaterialApp(home: HomePage());
 }
 
-class MyHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  static const _demos = <String, Widget>{
+    'Simple Calendar with Single Selection':
+        const SimpleCalendarWithSingleSelection(),
+    'Simple Calendar with Multi Selection':
+        const SimpleCalendarWithSingleSelection(),
+  };
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text('Calendar Sample App')),
-        body: Calendar(),
+  Widget build(BuildContext context) => DefaultTabController(
+        length: _demos.length,
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Calendar Sample App'),
+              bottom: TabBar(
+                  tabs: _demos.keys.map((_) => Tab(child: Text(_))).toList()),
+            ),
+            body: TabBarView(children: _demos.values.toList())),
       );
+}
+
+class SimpleCalendarWithSingleSelection extends StatefulWidget {
+  const SimpleCalendarWithSingleSelection();
+
+  @override
+  _SimpleCalendarWithSingleSelectionState createState() =>
+      _SimpleCalendarWithSingleSelectionState();
+}
+
+class _SimpleCalendarWithSingleSelectionState
+    extends State<SimpleCalendarWithSingleSelection> {
+  final _calendarKey = GlobalKey<CalendarState>();
+  final _calendarWidthController = TextEditingController(text: '300');
+  final _calendarHeightController = TextEditingController(text: '300');
+  final _columnsController = TextEditingController(text: '1');
+  final _rowsController = TextEditingController(text: '1');
+  var _calendarWidth = 300;
+  var _calendarHeight = 300;
+  var _columns = 1;
+  int get columns => _columns > 0 ? _columns : 1;
+  var _rows = 1;
+  int get rows => _rows > 0 ? _rows : 1;
+  var _scrollDirection = Axis.horizontal;
+
+  var _displayed = DateTime.now();
+  DateTime _selected;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildIntSelector(TextEditingController controller, String labelText,
+            void Function(int value) setValue,
+            [enabled = true]) =>
+        SizedBox(
+          width: 200,
+          child: TextField(
+            controller: controller,
+            enabled: enabled,
+            inputFormatters: [
+              IntTextInputFormatter(minValue: 0, maxValue: 5000)
+            ],
+            decoration: InputDecoration(labelText: labelText),
+            onChanged: (_) => setState(() => setValue(int.tryParse(_) ?? 0)),
+          ),
+        );
+
+    Widget buildEnumSelector<T>(String title, Iterable<T> values, T value,
+            void Function(T value) setValue) =>
+        Row(mainAxisSize: MainAxisSize.min, children: [
+          Text(title,
+              style:
+                  TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)),
+          const SizedBox(width: 8),
+          DropdownButton<T>(
+            items: values
+                .map((_) => DropdownMenuItem<T>(
+                    value: _, child: Text(TextHelper.enumToString(_))))
+                .toList(),
+            value: value,
+            onChanged: (_) => setState(() => setValue(_)),
+          ),
+        ]);
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Wrap(spacing: 24, runSpacing: 16, children: [
+            // calendar width
+            buildIntSelector(_calendarWidthController, 'Calendar Width',
+                (_) => _calendarWidth = _),
+            // calendar height
+            buildIntSelector(_calendarHeightController, 'Calendar Height',
+                (_) => _calendarHeight = _),
+            // columns
+            buildIntSelector(
+                _columnsController, 'Columns', (_) => _columns = _),
+            // rows
+            buildIntSelector(_rowsController, 'Rows', (_) => _rows = _),
+
+            // scroll direction
+            buildEnumSelector<Axis>('Scroll Direction:', Axis.values,
+                _scrollDirection, (_) => _scrollDirection = _),
+
+            // dec
+            IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => _calendarKey.currentState?.dec()),
+
+            // inc
+            IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: () => _calendarKey.currentState?.inc()),
+          ]),
+        ),
+        Text(_displayed.toString()),
+        Expanded(
+          child: Center(
+            child: Container(
+              key: ValueKey(_scrollDirection),
+              constraints: BoxConstraints(
+                  maxWidth: _calendarWidth.toDouble() * columns,
+                  maxHeight: _calendarHeight.toDouble() * rows),
+              padding: const EdgeInsets.all(16),
+              child: Calendar(
+                key: _calendarKey,
+                columns: columns,
+                rows: rows,
+                displayDate: _displayed,
+                scrollDirection: _scrollDirection,
+                onDisplayDateChanged: (_) => setState(() => _displayed = _),
+                dayBuilder: (date, type, column, row) {
+                  final day = Calendar.buildDefaultDay(date, type, column, row,
+                      selected: _selected == date);
+                  return type == DayType.today || type == DayType.current
+                      ? InkResponse(
+                          child: day,
+                          onTap: () => setState(() => _selected = date))
+                      : day;
+                },
+                buildCalendarDecorator: (date, calendar) => Row(
+                  children: [
+                    Expanded(
+                      child: Column(children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text('${date.month} ${date.year}',
+                              style: TextStyle(color: Colors.grey)),
+                        ),
+                        Expanded(child: calendar),
+                      ]),
+                    ),
+                    const SizedBox(width: 24),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IntTextInputFormatter extends TextInputFormatter {
+  IntTextInputFormatter({this.minValue, this.maxValue});
+
+  final int minValue;
+  final int maxValue;
+
+  String format(String oldValue, String newValue) {
+    if (newValue?.isNotEmpty != true) return '';
+    if (newValue.contains('-')) return oldValue;
+
+    var i = int.tryParse(newValue);
+    if (i == null) return oldValue;
+    if (minValue != null && i < minValue) i = minValue;
+    if (maxValue != null && i > maxValue) i = maxValue;
+
+    return i.toString();
+  }
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    final value = format(oldValue.text, newValue.text);
+    return value != newValue.text
+        ? newValue.copyWith(
+            text: value,
+            selection: TextSelection.collapsed(offset: value.length))
+        : newValue.copyWith(text: value);
+  }
+}
+
+class TextHelper {
+  static String _camelToWords(String value) {
+    final codes = value.runes
+        .skip(1)
+        .map((_) => String.fromCharCode(_))
+        .map((_) => _.toUpperCase() == _ ? ' $_' : _)
+        .expand((_) => _.runes);
+
+    return value[0].toUpperCase() + String.fromCharCodes(codes);
+  }
+
+  static String enumToString(dynamic value) =>
+      value == null ? '' : _camelToWords(value.toString().split('.')[1]);
 }
--- lib/calendart.dart
+++ lib/calendart.dart
@@ -2,7 +2,325 @@ library multicalendar;
 
 import 'package:flutter/material.dart';
 
-class Calendar extends StatelessWidget {
+enum DayType { extraLow, current, today, extraHigh }
+typedef DayBuilder = Widget Function(
+    DateTime date, DayType type, int colunm, int row);
+typedef CalendarDecoratorBuilder = Widget Function(
+    DateTime displayDate, Widget calendar);
+
+class _Calendar extends StatelessWidget {
+  const _Calendar({
+    Key key,
+    this.displayDate,
+    this.firstDayOfWeekIndex,
+    this.showDaysOfWeek = true,
+    @required this.dayOfWeekBuilder,
+    @required this.dayBuilder,
+  })  : assert(showDaysOfWeek != null),
+        assert(dayOfWeekBuilder != null),
+        assert(dayBuilder != null),
+        super(key: key);
+
+  final DateTime displayDate;
+  final int firstDayOfWeekIndex;
+  final bool showDaysOfWeek;
+  final IndexedWidgetBuilder dayOfWeekBuilder;
+  final DayBuilder dayBuilder;
+
   @override
-  Widget build(BuildContext context) => Container(color: Colors.yellow);
+  Widget build(BuildContext context) {
+    final firstDayOfWeekIndex = this.firstDayOfWeekIndex ??
+        MaterialLocalizations.of(context).firstDayOfWeekIndex ??
+        0;
+    final daysOfWeekFactor = showDaysOfWeek ? 1 : 0;
+
+    DateTime getDate(DateTime date) =>
+        DateTime(date.year, date.month, date.day);
+
+    final today = getDate(DateTime.now());
+    final date = this.displayDate ?? today;
+    final displayDate = DateTime(date.year, date.month);
+
+    var firstDate = DateTime(displayDate.year, displayDate.month, 1);
+    final shift =
+        (firstDate.weekday == DateTime.sunday ? 0 : firstDate.weekday) -
+            firstDayOfWeekIndex;
+    firstDate = firstDate.subtract(
+        Duration(days: shift < 0 ? shift + DateTime.daysPerWeek : shift));
+    if (firstDate.month == 2 &&
+        firstDate.day == 1 &&
+        DateTime(firstDate.year, 3, 1).difference(firstDate).inDays == 28) {
+      firstDate =
+          firstDate.subtract(const Duration(days: DateTime.daysPerWeek));
+    }
+
+    Widget buildDay(int column, int row) {
+      final date =
+          firstDate.add(Duration(days: row * DateTime.daysPerWeek + column));
+      final type = date.isBefore(displayDate)
+          ? DayType.extraLow
+          : date.month == displayDate.month
+              ? date == today ? DayType.today : DayType.current
+              : DayType.extraHigh;
+
+      return dayBuilder(date, type, column, row);
+    }
+
+    return Column(
+      children: Iterable.generate(6 + daysOfWeekFactor)
+          .map(
+            (row) => Expanded(
+              child: Row(
+                children: Iterable.generate(DateTime.daysPerWeek)
+                    .map(
+                      (column) => Expanded(
+                          child: showDaysOfWeek && row == 0
+                              ? dayOfWeekBuilder(
+                                  context,
+                                  (column + firstDayOfWeekIndex) %
+                                      DateTime.daysPerWeek)
+                              : buildDay(column, row - daysOfWeekFactor)),
+                    )
+                    .toList(),
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class Calendar extends StatefulWidget {
+  const Calendar({
+    Key key,
+    this.displayDate,
+    this.firstDayOfWeekIndex,
+    this.showDaysOfWeek = true,
+    this.dayOfWeekBuilder = buildDefaultDayOfWeek,
+    this.dayBuilder = buildDefaultDay,
+    this.onDisplayDateChanged,
+    this.columns = 1,
+    this.rows = 1,
+    this.scrollDirection = Axis.horizontal,
+    this.buildCalendarDecorator,
+  })  : assert(showDaysOfWeek != null),
+        assert(dayOfWeekBuilder != null),
+        assert(dayBuilder != null),
+        assert(columns > 0),
+        assert(rows > 0),
+        assert(scrollDirection != null),
+        super(key: key);
+
+  final DateTime displayDate;
+  final int firstDayOfWeekIndex;
+  final bool showDaysOfWeek;
+  final IndexedWidgetBuilder dayOfWeekBuilder;
+  final DayBuilder dayBuilder;
+  final ValueChanged<DateTime> onDisplayDateChanged;
+  final int columns;
+  final int rows;
+  final Axis scrollDirection;
+  final CalendarDecoratorBuilder buildCalendarDecorator;
+
+  @override
+  CalendarState createState() => CalendarState(displayDate);
+
+  static Widget buildDefaultDayOfWeek(BuildContext context, int index) =>
+      Center(
+          child: Text(MaterialLocalizations.of(context).narrowWeekdays[index],
+              style: TextStyle(color: Colors.blueAccent)));
+
+  static Widget buildDefaultDay(
+          DateTime date, DayType type, int column, int row,
+          {bool selected = false}) =>
+      AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        child: Center(
+          child: Text(
+            date.day.toString(),
+            style: type == DayType.current && !selected
+                ? null
+                : TextStyle(
+                    color: selected || type == DayType.today
+                        ? Colors.blueAccent
+                        : Colors.grey,
+                    decoration: type == DayType.today
+                        ? TextDecoration.underline
+                        : null),
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: selected ? Colors.blueAccent.withOpacity(0.1) : null,
+          border: Border.all(
+              color: selected
+                  ? Colors.blueAccent
+                  : Colors.blueAccent.withOpacity(0.0)),
+          borderRadius: BorderRadius.circular(9999),
+        ),
+      );
+}
+
+class CalendarState extends State<Calendar> {
+  CalendarState(DateTime displayDate)
+      : _displayDate = _getMonthDate(displayDate);
+  static const _itemsBefore = 2;
+
+  UniqueKey _listKey;
+  ScrollController _controller;
+  DateTime _displayDate;
+  double _calendarWidth;
+  double _calendarHeight;
+  double get _lenght => widget.scrollDirection == Axis.horizontal
+      ? _calendarWidth
+      : _calendarHeight;
+
+  static DateTime _getMonthDate(DateTime date) {
+    final d = date ?? DateTime.now();
+    return DateTime(d.year, d.month);
+  }
+
+  @override
+  void didUpdateWidget(Calendar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final displayDate = _getMonthDate(widget.displayDate);
+    if ((_getMonthDate(oldWidget.displayDate) != displayDate &&
+            _displayDate != displayDate) ||
+        oldWidget.scrollDirection != widget.scrollDirection ||
+        oldWidget.columns != widget.columns ||
+        oldWidget.rows != widget.rows) {
+      _listKey = null;
+      _reset(displayDate);
+    }
+  }
+
+  void _reset(DateTime date) => setState(() {
+        _controller.dispose();
+        _controller = null;
+        _displayDate = date;
+      });
+
+  void _move(double offset) =>
+      _controller.animateTo(_controller.position.pixels + offset,
+          duration: const Duration(milliseconds: 300), curve: Curves.bounceOut);
+
+  void inc() => _move(_lenght);
+  void dec() => _move(-_lenght);
+
+  DateTime _indexToDate(int index, [int cross = 0]) {
+    final crossFactor =
+        widget.scrollDirection == Axis.horizontal ? 1 : widget.columns;
+    final monthCount = _displayDate.year * DateTime.monthsPerYear +
+        _displayDate.month -
+        _itemsBefore * crossFactor -
+        1 +
+        index * crossFactor +
+        cross;
+    final month = (monthCount + 1) % DateTime.monthsPerYear;
+    return DateTime(
+        monthCount ~/ DateTime.monthsPerYear, month == 0 ? 12 : month);
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      LayoutBuilder(builder: (context, constrants) {
+        _calendarWidth = constrants.maxWidth / widget.columns;
+        _calendarHeight = constrants.maxHeight / widget.rows;
+        final scrollDirection = widget.scrollDirection;
+        final horizontal = scrollDirection == Axis.horizontal;
+
+        return NotificationListener<ScrollEndNotification>(
+          onNotification: (_) {
+            final date = _indexToDate(_controller.position.pixels ~/ _lenght);
+            if (date != _displayDate) {
+              _reset(date);
+              if (widget.onDisplayDateChanged != null) {
+                widget.onDisplayDateChanged(date);
+              }
+            }
+            return true;
+          },
+          child: ListView.builder(
+            key: _listKey ??= UniqueKey(),
+            controller: _controller ??=
+                ScrollController(initialScrollOffset: _itemsBefore * _lenght),
+            physics: _SnapScrollPhysics(itemSize: _lenght),
+            scrollDirection: scrollDirection,
+            itemBuilder: (context, index) {
+              Widget build(int index, [int cross = 0]) {
+                final date = _indexToDate(index, cross);
+                final calendar = _Calendar(
+                  displayDate: date,
+                  firstDayOfWeekIndex: widget.firstDayOfWeekIndex,
+                  showDaysOfWeek: widget.showDaysOfWeek,
+                  dayOfWeekBuilder: widget.dayOfWeekBuilder,
+                  dayBuilder: widget.dayBuilder,
+                );
+                return SizedBox(
+                  width: _calendarWidth,
+                  height: _calendarHeight,
+                  child: widget.buildCalendarDecorator == null
+                      ? calendar
+                      : widget.buildCalendarDecorator(date, calendar),
+                );
+              }
+
+              print(widget.displayDate);
+              print(index);
+
+              final calendars =
+                  Iterable.generate(horizontal ? widget.rows : widget.columns)
+                      .map((_) => build(
+                          horizontal ? index + _ * widget.columns : index,
+                          horizontal ? 0 : _))
+                      .toList();
+              return calendars.length == 1
+                  ? calendars[0]
+                  : horizontal
+                      ? Column(children: calendars)
+                      : Row(children: calendars);
+            },
+          ),
+        );
+      });
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+}
+
+class _SnapScrollPhysics extends ScrollPhysics {
+  const _SnapScrollPhysics({ScrollPhysics parent, @required this.itemSize})
+      : assert(itemSize > 0),
+        super(parent: parent);
+
+  final double itemSize;
+
+  @override
+  _SnapScrollPhysics applyTo(ScrollPhysics ancestor) =>
+      _SnapScrollPhysics(parent: buildParent(ancestor), itemSize: itemSize);
+
+  @override
+  Simulation createBallisticSimulation(
+      ScrollMetrics position, double velocity) {
+    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
+        (velocity >= 0.0 && position.pixels >= position.maxScrollExtent)) {
+      return super.createBallisticSimulation(position, velocity);
+    }
+    final tolerance = this.tolerance;
+    final target = (position.pixels / itemSize +
+                ((velocity < -tolerance.velocity)
+                    ? -0.5
+                    : velocity > tolerance.velocity ? 0.5 : 0))
+            .roundToDouble() *
+        itemSize;
+    return target == position.pixels
+        ? null
+        : ScrollSpringSimulation(spring, position.pixels, target, velocity,
+            tolerance: tolerance);
+  }
+
+  @override
+  bool get allowImplicitScrolling => false;
 }
